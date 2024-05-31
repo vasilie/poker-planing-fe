a046b6945cf1167786ea6abadff992fe9b3764be
vasilie <vasilie.91@gmail.com>
2024-05-31 06:46:20 +0200

    Update appjs to accept https socket connections

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
 if (process.env.NODE_ENV === "production") {
   require('dotenv').config();
 } else {
   require("dotenv").config({ path: ".env.development" });
 }
 
-var app = require('http').createServer()
+let server;
 
-var io = module.exports.io = require("socket.io")(app, { cors: {
+if (process.env.NODE_ENV === "production") {
+  server = https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/www.vasilie.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/www.vasilie.com/fullchain.pem')
+  });
+} else {
+  server = http.createServer();
+}
+
+var io = module.exports.io = require("socket.io")(server, { cors: {
   origin: `${process.env.APP_URL}:${process.env.SOCKET_PORT}`,
   methods: ["GET", "POST"],
   }
@@ -16,6 +29,6 @@ const SocketManager = require('./SocketManager')
 
 io.on('connection', SocketManager)
 
-app.listen(process.env.SOCKET_PORT, ()=> {
+server.listen(process.env.SOCKET_PORT, ()=> {
   console.log('connected on port: '+ process.env.SOCKET_PORT);
 })
